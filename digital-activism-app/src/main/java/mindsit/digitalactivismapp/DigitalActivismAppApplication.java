b7ffb33e7fd12704958a685e866f0288bc0624c5
Rifle1 <philipe.barakat@yahoo.com>
2024-10-17 11:43:41 +0200

    Backend Initialization

---
+++ digital-activism-app/src/main/java/mindsit/digitalactivismapp/DigitalActivismAppApplication.java
@@ -0,0 +1,38 @@
+package mindsit.digitalactivismapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+
+import java.util.List;
+
+@SpringBootApplication
+public class DigitalActivismAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DigitalActivismAppApplication.class, args);
+    }
+
+    // ENABLE COMMUNICATION BETWEEN ANGULAR (FRONTEND) AND SPRING BOOT (BACKEND)
+    @Bean
+    public CorsFilter corsFilter() {
+        CorsConfiguration corsConfiguration = new CorsConfiguration();
+        corsConfiguration.setAllowCredentials(true);
+        corsConfiguration.setAllowedOrigins(List.of("http://localhost:4300"));
+        corsConfiguration.setAllowedHeaders(List.of("Origin", "Access-Control-Allow-Origin", "Content-Type",
+                "Accept", "Authorization", "Origin, Accept", "X-Requested-With",
+                "Access-Control-Request-Method", "Access-Control-Request-Headers", "File-Name"));
+        corsConfiguration.setExposedHeaders(List.of("Origin", "Content-Type", "Accept", "Authorization",
+                "Access-Control-Allow-Origin", "Access-Control-Allow-Origin", "Access-Control-Allow-Credentials", "File-Name"));
+        corsConfiguration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
+
+        corsConfiguration.setAllowedOriginPatterns(List.of("http://localhost:4300/"));
+
+        UrlBasedCorsConfigurationSource urlBasedCorsConfigurationSource = new UrlBasedCorsConfigurationSource();
+        urlBasedCorsConfigurationSource.registerCorsConfiguration("/**", corsConfiguration);
+        return new CorsFilter(urlBasedCorsConfigurationSource);
+    }
+}
---
+++ digital-activism-app/src/test/java/mindsit/digitalactivismapp/DigitalActivismAppApplicationTests.java
@@ -0,0 +1,13 @@
+package mindsit.digitalactivismapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DigitalActivismAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
